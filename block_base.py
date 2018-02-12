66cdf2e03afca9731ffef396a847110c74674ebf
David P Hilton <david.hilton.p@gmail.com>
2018-02-11 21:49:35 -0700

    use old-style string formatting (not f-strings)

--- block_base.py
+++ block_base.py
@@ -82,14 +82,16 @@ class BlockBase(AuthBase):
         # take action
         if name in blocked:
             with self._session.switch_badge():  # so self can be used in the exception message
-                raise AuthException(f"Read from '{name}' blocked for {self._session.badge} on {self}: {blocked}")
+                raise AuthException("Read from '{name}' blocked for {badge} on {self}: {blocked}".
+                                    format(name=name, badge=self._session.badge, self=self, blocked=blocked))
         return super().__getattribute__(name)
 
     def __setattr__(self, name, value):
         blocked = self.write_blocked_attrs()
         if name in blocked:
             with self._session.switch_badge():  # so self can be used in the exception message
-                raise AuthException(f"Write to '{name}' blocked for {self._session.badge} on {self}: {blocked}")
+                raise AuthException("Write to '{name}' blocked for {badge} on {self}: {blocked}".
+                                    format(name=name, badge=self._session.badge, self=self, blocked=blocked))
         return super().__setattr__(name, value)
 
     def _bypass_block(self):
