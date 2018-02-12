39c48e5e6a428378ff3856c903ac5bd3f45ade5d
David P Hilton <david.hilton.p@gmail.com>
2018-02-12 02:51:36 -0700

    added package versioning

--- src/sqlalchemy_auth/__init__.py
+++ src/sqlalchemy_auth/__init__.py
@@ -1,3 +1,11 @@
+from pkg_resources import get_distribution, DistributionNotFound
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    __version__ = 'unknown'
+
+
 from .utils import ALLOW, DENY, AuthException
 from .auth_base import AuthBase
 from .block_base import BlockBase
